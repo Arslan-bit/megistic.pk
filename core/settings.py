686b43e976406f4f6922532f785e321969eeb9b7
Muhammad Arslan <56925454+Arslan-bit@users.noreply.github.com>
2023-04-11 14:59:13 +0500

    data base connect

--- core/settings.py
+++ core/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 deterministic=True
 
@@ -149,7 +149,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 
 MEDIA_URL = 'media/'
