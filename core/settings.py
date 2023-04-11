7ad3be8d306b429427d7a4c67d5057703787819c
Muhammad Arslan <56925454+Arslan-bit@users.noreply.github.com>
2023-04-11 13:46:57 +0500

    static

--- core/settings.py
+++ core/settings.py
@@ -145,8 +145,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles_build', 'core/static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static')]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
